563362b3da252b62b29b8116e7c8c91eab27ce73
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-22 21:17:59 -0300

    added some texts

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,31 @@
 'use strict';
 
-module.exports = (app) => {
+const texts = [
+	'lepidopterology',
+	'damp-dirt-dust',
+	'musings-on-museums'
+];
 
-	// home page
-	app.get('/', (req, res) => {
-		res.render('index.ejs');
-	});
+module.exports = (app) => {
 
 	// installer bootstrapper
 	app.get('/boot', (req, res) => {
 		res.redirect('https://raw.githubusercontent.com/keggsmurph21/etc/master/installers/live/bootstrap.sh');
+	});
+
+	// texts :)
+	app.get('/texts', (req, res) => {
+		res.render('texts/index.ejs');
+	});
+	texts.forEach((text) => {
+		app.get(`/texts/${text}`, (req, res) => {
+			res.render(`texts/${text}.ejs`);
+		});
+	});
+
+	// home page
+	app.get('/', (req, res) => {
+		res.render('index.ejs');
 	});
 
 };
