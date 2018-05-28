cb2ed12ebd7bff6e7fc8e21968df7ee92c708ac9
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-28 19:35:36 -0300

    add example callback json

--- src/groupme-bot.js
+++ src/groupme-bot.js
@@ -34,6 +34,24 @@ module.exports = {
   },
 
   callback: (json) => {
+    /*
+     * responses look like this:
+     *  {
+     *    "attachments": [],
+     *    "avatar_url": "https://i.groupme.com/123456789",
+     *    "created_at": 1302623328,
+     *    "group_id": "1234567890",
+     *    "id": "1234567890",
+     *    "name": "John",
+     *    "sender_id": "12345",
+     *    "sender_type": "user",
+     *    "source_guid": "GUID",
+     *    "system": false,
+     *    "text": "Hello world ☃☃",
+     *    "user_id": "1234567890"
+     *  }
+     */
+
     console.log('callback():', json);
     let data = { code:500, message:'callback() Not implemented' };
     return data;
