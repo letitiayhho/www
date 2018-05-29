b17beb0822ba8722e2312407b78410f3a9d7eea6
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-05-29 00:26:07 -0300

    commiting for deploy

--- src/routes.js
+++ src/routes.js
@@ -24,12 +24,12 @@ module.exports = (app) => {
 	// WhatsApp bot callback
 	app.put('/bot/identify', (req, res) => {
 		console.log('PUT /bot/identify');
-		const data = bot.identify(req);
+		const data = bot.identify(req.body);
 		res.status(data.code).json(data.message);
 	});
 	app.all('/bot/callback', (req, res) => {
 		console.log('<ALL> /bot/callback');
-		const data = bot.callback(req.body);
+		const data = bot.callback(req);
 		res.status(data.code).json(data.message);
 	});
 	app.get('/bot/queue', (req, res) => {
